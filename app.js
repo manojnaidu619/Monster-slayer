d8e265e1a11dd77cd044e7f5caeef3708179bb5a
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-11 22:18:35 +0530

    Attack function working

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ new Vue({
       this.monsterHealth = 100
     },
     attack: function(){
-          var monsterDamage = Math.floor(Math.random() * 10)
+          var monsterDamage = this.damage()
           this.monsterHealth -= monsterDamage
-
-          var myDamage = Math.floor(Math.random() * 10)
+          var myDamage = this.damage()
           this.myHealth -= myDamage
+          this.healthCheck()
     },
     specialAttack: function(){
 
@@ -26,6 +26,32 @@ new Vue({
     },
     giveUp: function(){
 
+    },
+    damage: function(){
+      return Math.floor(Math.random() * 10)
+    },
+    healthCheck: function(){
+      if(this.myHealth >= 0 && this.monsterHealth < 0){
+        if(confirm("Yeah, you Won! Wanna try again?")){
+          this.retryGame()
+        }else{
+          this.quitGame()
+        }
+      }else if(this.myHealth <= 0 && this.monsterHealth > 0){
+        if(confirm("OOPS, You Lost! Wanna try again?")){
+          this.retryGame()
+        }else{
+          this.quitGame()
+        }
+      }
+    },
+    retryGame: function(){
+      this.myHealth = this.monsterHealth = 100
+      this.gameStart = true
+    },
+    quitGame: function(){
+      this.myHealth = this.monsterHealth = 100
+      this.gameStart = false
     }
   }
 });
